e8693566045b9200ec6a976bbb02435eac2b4a11
Yusuf Putra <yusufputra@Yusufs-iMac.local>
2019-05-25 04:42:53 +0700

    tambah filter

--- src/page/ListMitra.js
+++ src/page/ListMitra.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
 import { Layout, Breadcrumb, Table } from 'antd';
+import { Link } from 'react-router-dom';
 import Axios from 'axios';
+import QRCode from 'qrcode.react';
 const { Content } = Layout;
 
 
 export default class ListMitra extends Component {
     state = {
-        loading : false,
-        data : []
+        loading: false,
+        data: []
     }
     componentWillMount() {
-        this.setState({loading: true});
+        this.setState({ loading: true });
+        Axios.get('https://backend-bem.herokuapp.com/api/web/public/getMitra').then(ress => {
+            console.log(ress.data);
+            this.setState({ data: ress.data });
+            this.setState({ loading: false });
+        })
+    }
+    delete = (id) => {
+        console.log(id)
+        this.setState({ loading: true });
+        const body = {
+            id: id
+        }
         const token = JSON.parse(localStorage.token);
-        Axios.get('https://backend-bem.herokuapp.com/api/web/protected/getRekapData', {
+        Axios.post('https://backend-bem.herokuapp.com/api/web/protected/deleteMitra', body, {
             headers: {
                 authorization: 'bearer ' + token.token
             }
-        }).then(ress=>{
-            console.log(ress.data);
-            this.setState({data:ress.data});
-            this.setState({loading: false});
+        }).then(ress => {
+            alert("Berhasil Menghapus");
+            this.setState({ loading: true });
+            window.location.reload();
+        }).catch(err => {
+            alert("Gagal Menghapus. error: " + err);
+            this.setState({ loading: true });
         })
     }
+
     render() {
         const column = [
             {
-                title: 'NIM',
-                dataIndex: 'nim',
-                sorter: (a, b) => a.nim - b.nim,
-                sortDirections: ['descend', 'ascend'],
-            },
-            {
                 title: 'Kode Mitra',
-                dataIndex: 'kodeMitra',
-                sorter: (a, b) => a.kodeMitra - b.kodeMitra,
+                dataIndex: 'key',
+                sorter: (a, b) => a.key - b.key,
                 sortDirections: ['descend', 'ascend'],
             },
             {
@@ -41,24 +53,45 @@ export default class ListMitra extends Component {
                 dataIndex: 'namaMitra',
             },
             {
-                title: 'Timestamp',
-                dataIndex: 'timestamp',
-                sorter: (a, b) => a.timestamp - b.timestamp,
-                sortDirections: ['descend', 'ascend'],
+                title: 'Syarat',
+                dataIndex: 'syarat',
+                render: (html) => {
+                    return <div dangerouslySetInnerHTML={{ __html: html }} />
+                }
             },
             {
-                title: 'Bulan',
-                dataIndex: 'bulan',
-                sorter: (a, b) => a.bulan - b.bulan,
-                sortDirections: ['descend', 'ascend'],
+                title: 'Benefit',
+                dataIndex: 'benefit',
+                render: (html) => {
+                    return <div dangerouslySetInnerHTML={{ __html: html }} />
+                }
             },
             {
-                title: 'Tahun',
-                dataIndex: 'tahun',
-                sorter: (a, b) => a.tahun - b.tahun,
-                sortDirections: ['descend', 'ascend'],
+                title: 'Author',
+                dataIndex: 'nimAuthor',
+            },
+            {
+                title: 'Link',
+                dataIndex: 'key',
+                render: (html) => {
+                    return <QRCode id={"canvas"+html} value={"https://bemfilkom.ub.ac.id/bismit?beli=" + html} onClick={() => {
+                        let canvas = document.getElementById("canvas"+html);
+                        let link = document.createElement('a');
+                        link.download = html+".png";
+                        link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");;
+                        link.click();
+                    }} />
+                }
+            },
+            {
+                title: 'Action',
+                dataIndex: 'key',
+                render: (html) => {
+                    return <Link onClick={() => this.delete(html)}>Delete</Link>
+                }
             }
         ];
+
         return (
             <div>
                 <Content style={{ margin: '0 16px' }}>
@@ -67,9 +100,9 @@ export default class ListMitra extends Component {
                         <Breadcrumb.Item>List Mitra</Breadcrumb.Item>
                     </Breadcrumb>
                     <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                        List Rekap Data
-                        {this.state.loading && (<center><img src="https://i.imgur.com/AfRTFrY.gif" width="100px" alt="Loading..."/></center>)}
-                        <Table columns={column} dataSource={this.state.data}/>
+                        List Mitra
+                        {this.state.loading && (<center><img src="https://i.imgur.com/AfRTFrY.gif" width="100px" alt="Loading..." /></center>)}
+                        <Table columns={column} dataSource={this.state.data} />
                     </div>
                 </Content>
             </div>
--- src/page/RekapData.js
+++ src/page/RekapData.js
@@ -1,51 +1,39 @@
 import React, { Component } from 'react';
 import { Layout, Breadcrumb, Table } from 'antd';
-import { Link } from 'react-router-dom';
 import Axios from 'axios';
-import QRCode from 'qrcode.react';
 const { Content } = Layout;
 
 
 export default class RekapData extends Component {
     state = {
-        loading: false,
-        data: []
+        loading : false,
+        data : []
     }
     componentWillMount() {
-        this.setState({ loading: true });
-        Axios.get('https://backend-bem.herokuapp.com/api/web/public/getMitra').then(ress => {
-            console.log(ress.data);
-            this.setState({ data: ress.data });
-            this.setState({ loading: false });
-        })
-    }
-    delete = (id) => {
-        console.log(id)
-        this.setState({ loading: true });
-        const body = {
-            id: id
-        }
+        this.setState({loading: true});
         const token = JSON.parse(localStorage.token);
-        Axios.post('https://backend-bem.herokuapp.com/api/web/protected/deleteMitra',body,{
+        Axios.get('https://backend-bem.herokuapp.com/api/web/protected/getRekapData', {
             headers: {
                 authorization: 'bearer ' + token.token
             }
         }).then(ress=>{
-            alert("Berhasil Menghapus");
-            this.setState({ loading: true });
-            window.location.reload();
-        }).catch(err=>{
-            alert("Gagal Menghapus. error: " + err);
-            this.setState({ loading: true });
+            console.log(ress.data);
+            this.setState({data:ress.data});
+            this.setState({loading: false});
         })
     }
-
     render() {
         const column = [
             {
+                title: 'NIM',
+                dataIndex: 'nim',
+                sorter: (a, b) => a.nim - b.nim,
+                sortDirections: ['descend', 'ascend'],
+            },
+            {
                 title: 'Kode Mitra',
-                dataIndex: 'key',
-                sorter: (a, b) => a.key - b.key,
+                dataIndex: 'kodeMitra',
+                sorter: (a, b) => a.kodeMitra - b.kodeMitra,
                 sortDirections: ['descend', 'ascend'],
             },
             {
@@ -53,39 +41,24 @@ export default class RekapData extends Component {
                 dataIndex: 'namaMitra',
             },
             {
-                title: 'Syarat',
-                dataIndex: 'syarat',
-                render: (html) => {
-                    return <div dangerouslySetInnerHTML={{ __html: html }} />
-                }
+                title: 'Timestamp',
+                dataIndex: 'timestamp',
+                sorter: (a, b) => a.timestamp - b.timestamp,
+                sortDirections: ['descend', 'ascend'],
             },
             {
-                title: 'Benefit',
-                dataIndex: 'benefit',
-                render: (html) => {
-                    return <div dangerouslySetInnerHTML={{ __html: html }} />
-                }
+                title: 'Bulan',
+                dataIndex: 'bulan',
+                sorter: (a, b) => a.bulan - b.bulan,
+                sortDirections: ['descend', 'ascend'],
             },
             {
-                title: 'Author',
-                dataIndex: 'nimAuthor',
-            },
-            {
-                title: 'Link',
-                dataIndex: 'key',
-                render: (html) => {
-                    return <QRCode value={"https://bemfilkom.ub.ac.id/bismit?beli=" + html} />
-                }
-            }, 
-            {
-                title: 'Action',
-                dataIndex: 'key',
-                render: (html) => {
-                    return <Link onClick={()=>this.delete(html)}>Delete</Link>
-                }
+                title: 'Tahun',
+                dataIndex: 'tahun',
+                sorter: (a, b) => a.tahun - b.tahun,
+                sortDirections: ['descend', 'ascend'],
             }
         ];
-
         return (
             <div>
                 <Content style={{ margin: '0 16px' }}>
@@ -94,9 +67,9 @@ export default class RekapData extends Component {
                         <Breadcrumb.Item>List</Breadcrumb.Item>
                     </Breadcrumb>
                     <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                        List Mitra
-                        {this.state.loading && (<center><img src="https://i.imgur.com/AfRTFrY.gif" width="100px" alt="Loading..." /></center>)}
-                        <Table columns={column} dataSource={this.state.data} />
+                        List Rekap Data
+                        {this.state.loading && (<center><img src="https://i.imgur.com/AfRTFrY.gif" width="100px" alt="Loading..."/></center>)}
+                        <Table columns={column} dataSource={this.state.data}/>
                     </div>
                 </Content>
             </div>
