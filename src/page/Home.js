faead4a98fe1d2b53d7dc5ad1d1bcbaa412c11a0
Yusuf Putra <yusufputra@Yusufs-iMac.local>
2019-05-25 05:19:10 +0700

    admin done

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 import { Layout, Breadcrumb } from 'antd';
+import Axios from 'axios';
+import CanvasJSReact from '../asset/js/canvasjs.react';
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 const { Content } = Layout;
 
+
+
+
 export default class Home extends Component {
+    state = {
+        loading: false,
+        data: []
+    }
+    componentWillMount() {
+        this.setState({ loading: true });
+        const token = JSON.parse(localStorage.token);
+        Axios.get('https://backend-bem.herokuapp.com/api/web/protected/getRekapDataByMonth', {
+            headers: {
+                authorization: 'bearer ' + token.token
+            }
+        }).then(ress => {
+            console.log(ress.data);
+            this.setState({ data: ress.data });
+            this.setState({ loading: false });
+        })
+    }
+
     render() {
+        const options = {
+            title: {
+                text: "Statistik Perbulan"
+            },
+            animationEnabled: true,
+            data: [
+                {
+                    // Change type to "doughnut", "line", "splineArea", etc.
+                    type: "column",
+                    dataPoints: this.state.data
+                }
+            ]
+        }
         return (
             <div>
                 <Content style={{ margin: '0 16px' }}>
@@ -12,7 +49,11 @@ export default class Home extends Component {
                         <Breadcrumb.Item>Chart</Breadcrumb.Item>
                     </Breadcrumb>
                     <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                        Halaman Utama
+                        List Rekap Data
+                        {this.state.loading && (<center><img src="https://i.imgur.com/AfRTFrY.gif" width="100px" alt="Loading..." /></center>)}
+                        <CanvasJSChart options={options}
+                            onRef={ref => this.chart = ref}
+                        />
                     </div>
                 </Content>
             </div>
